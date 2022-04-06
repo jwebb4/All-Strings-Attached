386b647e68062a2bb13e2b541d80b5203b16142e
jwebb4 <46533437+jwebb4@users.noreply.github.com>
2022-04-05 19:51:30 -0500

    Fixed compiler issues

--- saveDataWithPreferences.ino
+++ saveDataWithPreferences.ino
@@ -1,15 +1,14 @@
 /*
  ESP32 startup counter example with Preferences library.
-
  This simple example demonstrates using the Preferences library to store how many times the ESP32 module has booted. 
  The Preferences library is a wrapper around the Non-volatile storage on ESP32 processor.
-
  created for arduino-esp32 09 Feb 2017 by Martin Sloup (Arcao)
  
  Complete project details at https://RandomNerdTutorials.com/esp32-save-data-permanently-preferences/
 */
 
 // yyyymmddhhmmssF
+#include <ESP32Time.h>
 #include <TimeLib.h>
 #include <Preferences.h>
 #include <BLEDevice.h>
@@ -19,13 +18,18 @@
 
 #define Threshold 40
 #define MAX_CHARS 15
-
+#define x_sensor_pin 25  //D2
+#define nx_sensor_pin 14 //D6
+#define y_sensor_pin 26  //D3
+#define ny_sensor_pin 13 //D7
+#define z_sensor_pin 0   //D5
+#define nz_sensor_pin 2  //D9
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b" // Currently using
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8" // still in use but need to switch
-
 #define dateCharacteristicUUID  "c84b5feb-a12f-45bb-a3ff-a6adce24f69e"
 
 Preferences preferences;
+ESP32Time rtc;
 
 BLECharacteristic dateCharacteristics("c84b5feb-a12f-45bb-a3ff-a6adce24f69e",BLECharacteristic::PROPERTY_NOTIFY);
 BLEDescriptor dateDescriptor(BLEUUID((uint16_t)0x2902));
@@ -34,26 +38,49 @@ String timeDifference = "timeDifference";
 String baseNameDataSpace = "dtStorage";
 String tempStartData = "";
 String tempEndData = "";
-int count = 1;
 
+volatile int Interupt_Counter = 0;
+volatile int interruptCounter;
+int totalInterruptCounter;
+int count = 1;
+int minutes;
+int timeActive;
+int x_lastState = 0;
+int y_lastState = 0;
+int z_lastState = 0;
+int nx_lastState = 0;
+int ny_lastState = 0;
+int nz_lastState = 0;
+uint32_t timePlayed = 0;
+uint32_t BUTTON_PIN_BITMASK = 0x2000; //2^0 + 2^13 + 2^14 in Hex
+uint8_t sensor = 0;
+uint8_t x_tilt = 0;
+uint8_t y_tilt = 0;
+uint8_t z_tilt = 0;
+uint8_t nx_tilt = 0;
+uint8_t ny_tilt = 0;
+uint8_t nz_tilt = 0;
+
+String translate = "";
+double dateValue;
+
+RTC_DATA_ATTR bool first_use = true;
+
+bool oldDeviceConnected = false;
+bool AllSensorsChecked = 0;
+bool SensorChanged = 0;
+bool deviceConnected = false;
+bool DetectMotionAndSound = 0;
 bool isTouched = false;
 bool isStorageFull = false; // most liekly set a limit of 5 start/end saves
 bool isReadySendData = false;
 bool isClearingData = false;
 
-volatile int interruptCounter;
-int totalInterruptCounter;
-int minutes;
-int timeActive;
-
-String translate = "";
-double dateValue;
-
-bool deviceConnected = false;
-bool oldDeviceConnected = false;
-
 touch_pad_t touchPin;
+
 hw_timer_t * timer = NULL;
+hw_timer_t* SleepTimer = NULL;
+
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 
 class MyServerCallbacks: public BLEServerCallbacks {
@@ -73,6 +100,76 @@ void IRAM_ATTR onTimer() {
   portEXIT_CRITICAL_ISR(&timerMux);
 }
 
+  
+
+void IRAM_ATTR onDeepSleepTimer() {
+  Interupt_Counter++;
+  Serial.println(String(Interupt_Counter));
+  if (Interupt_Counter >= 20) {
+      Serial.println("start deep sleep");
+      esp_sleep_enable_ext1_wakeup(BUTTON_PIN_BITMASK,ESP_EXT1_WAKEUP_ANY_HIGH);
+      esp_bt_controller_disable();
+      esp_deep_sleep_start();
+  }
+}
+
+void SensorCheck() {
+  AllSensorsChecked = 0;
+  SensorChanged = 0;
+  do {
+    if (x_tilt != x_lastState){
+      x_lastState = x_tilt;
+      SensorChanged = 1;
+    }else if(y_tilt != y_lastState){
+      y_lastState = y_tilt;
+      SensorChanged = 1;
+    }else if(z_tilt != z_lastState){
+      z_lastState = z_tilt;
+      SensorChanged = 1;
+    }else if(nx_tilt != nx_lastState){
+      nx_lastState = nx_tilt;
+      SensorChanged = 1;
+    }else if(ny_tilt != ny_lastState){
+      ny_lastState = ny_tilt;
+      SensorChanged = 1;
+    }else if(nz_tilt != nz_lastState){
+      nz_lastState = nz_tilt;
+      SensorChanged = 1;
+    }else {
+      AllSensorsChecked = 1;
+    }
+  }while (AllSensorsChecked == 0);
+
+  if(SensorChanged){
+    WakeupChange();
+  }
+}
+
+void WakeupChange() { // initlize all wakeup sensors
+  BUTTON_PIN_BITMASK = 0x0;
+  if (!x_lastState) {
+    BUTTON_PIN_BITMASK += pow(2,x_sensor_pin);
+  }
+  if (!y_lastState) {
+    BUTTON_PIN_BITMASK += pow(2,y_sensor_pin);
+  }
+  if (!z_lastState) {
+    BUTTON_PIN_BITMASK += pow(2,z_sensor_pin);
+  }
+  if (!nx_lastState) {
+    BUTTON_PIN_BITMASK += pow(2,nx_sensor_pin);
+  }
+  if (!ny_lastState) {
+    BUTTON_PIN_BITMASK += pow(2,ny_sensor_pin);
+  }
+  if (!nz_lastState) {
+    BUTTON_PIN_BITMASK += pow(2,nz_sensor_pin);
+  }
+  if (BUTTON_PIN_BITMASK == 0x0){
+    BUTTON_PIN_BITMASK += pow(2,nz_sensor_pin);
+  }
+  esp_sleep_enable_ext1_wakeup(BUTTON_PIN_BITMASK,ESP_EXT1_WAKEUP_ANY_HIGH);
+}
 
 void callback(){
 }
@@ -231,6 +328,10 @@ void setup() {
   Serial.begin(115200);
   Serial.println();
 
+  SleepTimer = timerBegin(0,80,true);
+  timerAttachInterrupt(SleepTimer, &onDeepSleepTimer, true);
+  timerAlarmWrite(SleepTimer,1000000,true);
+ 
   timer = timerBegin(0, 80, true);
   timerAttachInterrupt(timer, &onTimer, true);
   timerAlarmWrite(timer, 1000000, true);
@@ -276,6 +377,30 @@ void setup() {
   
   BLEDevice::startAdvertising();
 
+// initialize sensors
+// -------------------------------------------
+    pinMode(25,INPUT);
+    pinMode(14,INPUT_PULLDOWN);
+    pinMode(26,INPUT);
+    pinMode(13,INPUT);
+    pinMode(0,INPUT_PULLDOWN);
+    pinMode(2, INPUT);
+    
+    x_tilt = !digitalRead(x_sensor_pin); // 0x00200000
+    y_tilt = !digitalRead(y_sensor_pin); // 0x00400000
+    z_tilt = !digitalRead(z_sensor_pin); // 0x0001
+    nx_tilt = digitalRead(nx_sensor_pin); // 0x4000
+    ny_tilt = digitalRead(ny_sensor_pin); // 0x2000
+    nz_tilt = digitalRead(nz_sensor_pin); // 0x0004  
+  
+    x_lastState = x_tilt;
+    y_lastState = y_tilt;
+    z_lastState = z_tilt;
+    nx_lastState = nx_tilt;
+    ny_lastState = ny_tilt;
+    nz_lastState = nz_tilt;
+ // -------------------------------------------
+ 
   for(int i = 1; i <= 10; i++){
         //
         String namingStorage = baseNameDataSpace + String(i); // mmddyyyyhhmmf
@@ -312,6 +437,16 @@ void setup() {
 }
 
 void loop() {
+ 
+  x_tilt = !digitalRead(x_sensor_pin); // 0x00200000
+  y_tilt = !digitalRead(y_sensor_pin); // 0x00400000
+  z_tilt = !digitalRead(z_sensor_pin); // 0x0001
+  nx_tilt = digitalRead(nx_sensor_pin); // 0x4000
+  ny_tilt = digitalRead(ny_sensor_pin); // 0x2000
+  nz_tilt = digitalRead(nz_sensor_pin); // 0x0004   
+  SensorCheck();
+ 
+ 
   if (deviceConnected) {
     //Serial.println("Connected");
     if(touchRead(T0) < 60){
